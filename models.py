f58664a49aa49133db0c6b09f3ef46d79dce031f
Benjamín Garzón <benjamin.garzon@gmail.com>
2021-07-20 12:54:46 +0200

    class cleaning and balancing

--- models.py
+++ models.py
@@ -12,24 +12,32 @@ from tensorflow.keras.layers import Input, Dense, Dropout, Lambda, Flatten
 import tensorflow as tf
 #Conv3D,GlobalAveragePooling3D, MaxPooling3D, 
 from util import euclidean_distance, contrastive_loss, plot_training, \
-    create_pairs
-from config import DEFAULT_SIAMESE_PARAMS
+    create_pairs, distance_ratio, plot_embedding, balance_labels, get_correct
+from config import DEFAULT_SIAMESE_PARAMS, PLOT_PATH
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import PredefinedSplit
+
 tf.keras.backend.clear_session()
 
-BASE_OUTPUT = "output"
-MODEL_PATH = os.path.sep.join([BASE_OUTPUT, "siamese_model"])
-PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot.png"])
-
 class SiameseNet():
-    def __init__(self, target_col, params = None):
+    def __init__(self, target_col, model_ref = None, params = None, logger = None, 
+                 shuffle = False):
         self.target_col = target_col
         if params is None:
             self.params = DEFAULT_SIAMESE_PARAMS
         else:
             self.params = params
+        self.model_ref = model_ref
+        self.acc_function = distance_ratio
+        self.logger = logger
+        self.shuffle = shuffle # shuffle data to have a baseline
+        self.trained = False
         
+    def log(self, message):
+        if self.logger:
+            self.logger.info(message)
+            
     def build(self, input_shape):
 	# second set of CONV => RELU => POOL => DROPOUT layers
 	#x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
@@ -57,7 +65,7 @@ class SiameseNet():
         #          Dense(embedding_dimension)]
         #sister = Sequential(layers)
         
-        self.sister.summary()
+        #self.sister.summary()
 
         X_1 = Input(shape=input_shape)
         X_2 = Input(shape=input_shape)
@@ -77,10 +85,10 @@ class SiameseNet():
             self.model.compile(loss=contrastive_loss, optimizer="adam",
                           metrics=["accuracy"])
 
-        self.model.summary()
+        #self.model.summary()
 
     def fit(self, X_pairs_train, label_train, X_pairs_test = None, 
-            label_test = None, plot = False):
+            label_test = None, plot = True):
         
         if label_test is not None:
             self.history = self.model.fit(
@@ -98,7 +106,9 @@ class SiameseNet():
         # plot the training history
         #print("[INFO] plotting training history...")
         if plot:
-            plot_training(self.history, PLOT_PATH, 
+            plot_file = os.path.join(PLOT_PATH, self.model_ref) if self.model_ref else None
+            plot_training(self.history, 
+                          plot_file, 
                           test = label_test is not None)
 
     def fit_file(self, train_file, test_file = None):         
@@ -113,35 +123,140 @@ class SiameseNet():
         None. 
 
         """
-        
-        train_data = pd.read_csv(train_file)
+        train_data, ntrials, nvalid, nruns = get_correct(pd.read_csv(train_file))
         self.feature_cols = [ x for x in train_data.columns if 'feature' in x ]
+
+        runs = np.unique(train_data.run)
+        if nruns < 4:
+            self.log('Skipping, less than 4 runs: %s'%train_file)
+            return None
+        else: 
+            self.log('%s has %d valid runs'%(train_file, len(runs)))
+        train_data = balance_labels(train_data, self.target_col)
+        
         y_train = train_data[self.target_col].values
         X_train = train_data[self.feature_cols].values
         self.trained_labels = np.unique(y_train)
+
+        if self.shuffle:
+            for run in runs:
+                y_train[train_data.run == run] = \
+                    np.random.permutation(y_train[train_data.run == run])    
 
         index_train, X_pairs_train, label_train = create_pairs(X_train, y_train)
  
         self.build(input_shape = (X_train.shape[1]) )
         
         if test_file is not None:
-            test_data = pd.read_csv(test_file)
-            y_test = test_data[self.target_col]
-            # remove labels that were not trained
-            X_test = test_data[self.feature_cols]
-            X_test = X_test.loc[y_test.isin(self.trained_labels), :].values
-            y_test = y_test.loc[y_test.isin(self.trained_labels)].values
-            
+
+            test_data, *_ = get_correct(pd.read_csv(test_file))
+            # remove labels that were not trained and balance
+            test_data = test_data.loc[test_data[self.target_col].isin(self.trained_labels)]
+            test_data = balance_labels(test_data, self.target_col)
+            X_test = test_data[self.feature_cols].values
+            y_test = test_data[self.target_col].values
+
             index_test, X_pairs_test, label_test = create_pairs(X_test, y_test)
         else:
             X_pairs_test, label_test = None, None
         
         self.fit(X_pairs_train, label_train, X_pairs_test, label_test)
-        #fitted_model = self.model.fit(X, y)
-        #return fitted_model
+        self.trained = True
 
-        # train the model
-    
+    def fit_and_predict_file_cv(self, data_file):         
+        """
+        Fit model to data, crossvalidating within run.
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None. 
+
+        """
+
+        data, ntrials, nvalid, nruns = get_correct(pd.read_csv(data_file))
+        self.feature_cols = [ x for x in data.columns if 'feature' in x ]
+
+#        valid_types = data.groupby(['run', 'seq_type']).iscorrect.sum()
+#        valid_runs = (valid_types >= MINCORRECT).groupby('run').sum() == NTYPES
+#        valid_runs = valid_runs.index[valid_runs]
+#        valid = np.logical_and(data.run.isin(valid_runs),
+#                               data.iscorrect)
+#        data = data[data.valid]
+        #print(data.groupby(['run', 'seq_type']).iscorrect.sum())
+        
+#        nvalid = np.sum(valid)
+#        nn = len(valid)
+#        nruns = len(valid_runs)
+        print(ntrials, nvalid, nruns)
+        if nruns < 4:
+            self.log('Skipping, less than 4 runs: %s'%data_file)
+            return np.nan
+        else: 
+            self.log('%s has %d valid runs'%(data_file, len(np.unique(data.run))))
+        data = balance_labels(data, self.target_col)
+        X = data[self.feature_cols].values
+        y = data[self.target_col].values
+
+        folds = PredefinedSplit(data.run)
+
+        self.build(input_shape = (X.shape[1]))
+        
+        scores = []
+#        embeddings = []
+#        X_test_list = []
+#        y_test_list = []
+        for i, (train_index, test_index) in enumerate(folds.split()):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            
+            if self.shuffle:
+                y_train = np.random.permutation(y_train)    
+                
+            index_train, X_pairs_train, label_train = create_pairs(X_train, y_train)
+            index_test, X_pairs_test, label_test = create_pairs(X_test, y_test)
+ 
+            self.fit(X_pairs_train, label_train, X_pairs_test, label_test)
+            embeddings = self.predict_embedding(X_test)
+            score = self.acc_function(embeddings, index_test, label_test)
+            scores.append(score)
+            plot_file = os.path.join(PLOT_PATH,  '%s-split%i'%(self.model_ref, i)) if self.model_ref else None
+            plot_embedding(embeddings, 
+                       y_test,
+                       plot_file = plot_file)
+            
+ #       X_test = np.concatenate(X_test_list)
+ #       y_test = np.concatenate(y_test_list)
+ #       index_test, X_pairs_test, label_test = create_pairs(X_test, y_test)
+         
+ #       score = self.acc_function(embeddings, index_test, label_test)
+ #       scores.append(score)
+ #       plot_file = os.path.join(PLOT_PATH,  '%s'%(self.model_ref)) if self.model_ref else None
+ #       plot_embedding(embeddings, 
+ #                      y_test,
+ #                      plot_file = plot_file)
+ 
+ #           X_test_list.append(X_test)
+ #           y_test_list.append(y_test)
+         
+ #           embeddings.append(self.predict_embedding(X_test))
+ #       embeddings = np.concatenate(embeddings, axis = 0)
+ #       X_test = np.concatenate(X_test_list)
+ #       y_test = np.concatenate(y_test_list)
+ #       index_test, X_pairs_test, label_test = create_pairs(X_test, y_test)
+         
+ #       score = self.acc_function(embeddings, index_test, label_test)
+ #       scores.append(score)
+ #       plot_file = os.path.join(PLOT_PATH,  '%s'%(self.model_ref)) if self.model_ref else None
+ #       plot_embedding(embeddings, 
+ #                      y_test,
+ #                      plot_file = plot_file)
+            
+        cv_score = np.mean(scores)
+        return(cv_score)
+        
     def predict_embedding(self, X):
         return self.sister.predict(X)
         
@@ -157,24 +272,21 @@ class SiameseNet():
         None. 
 
         """
-        test_data = pd.read_csv(test_file)
-        y_test = test_data[self.target_col]
-        # remove labels that were not trained
-        X_test = test_data[self.feature_cols]
-        X_test = X_test.loc[y_test.isin(self.trained_labels), :].values
-        y_test = y_test.loc[y_test.isin(self.trained_labels)].values
+
+        if not self.trained:
+            return np.nan
+        test_data, *_ = get_correct(pd.read_csv(test_file))
+
+        # remove labels that were not trained and balance
+        test_data = test_data.loc[test_data[self.target_col].isin(self.trained_labels)]
+        test_data = balance_labels(test_data, self.target_col)
+        X_test = test_data[self.feature_cols].values
+        y_test = test_data[self.target_col].values
+
 
         index_test, X_pairs_test, label_test = create_pairs(X_test, y_test)
 
         embeddings = self.predict_embedding(X_test)
-        same, different = [] , []
-        for index, (i, j) in enumerate(index_test):
-            dist = euclidean_distance([embeddings[[i], :], embeddings[[j], :]]).numpy()
-        
-            if label_test[index] == 1: #same class
-                same.append(dist)
-            else:
-                different.append(dist)
-            score = np.mean(np.array(different)) / np.mean(np.array(same))
-        return(score)
-        
+        score = self.acc_function(embeddings, index_test, label_test)
+
+        return(score)        
