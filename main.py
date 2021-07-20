a392a2b913e3d9342741b5d3d935fd62fd322c95
Benjamín Garzón <benjamin.garzon@gmail.com>
2021-07-20 12:52:42 +0200

    added experiments

--- main.py
+++ main.py
@@ -6,18 +6,25 @@ Created on Wed Jul 14 13:07:01 2021
 @author: benjamin.garzon@gmail.com
 """
 from config import FILES_DIR, TARGET_COL, TEST_LABEL, PARAM_GRID, NUM_CORES, \
-    OUTPUT_DIR
+    OUTPUT_DIR, DEFAULT_SIAMESE_PARAMS
 from models import SiameseNet
 from distance_estimator import Estimator
 import numpy as np
 import os
-from util import loop_params
+from util import loop_params, plot_results
 from joblib import Parallel, delayed
+import pickle
 
-#if __name__ == '__main__':
-def launch_experiment(files_dir, target_col, ModelClass, params = None):
+shuffle = False
+if shuffle:
+    results_file = os.path.join(OUTPUT_DIR, 'results_shuffled.pkl')
+else:
+    results_file = os.path.join(OUTPUT_DIR, 'results.pkl')
 
-    estimator = Estimator(files_dir, target_col, ModelClass, params)
+def launch_experiment(files_dir, target_col, ModelClass, params = None, 
+                      shuffle = False):
+
+    estimator = Estimator(files_dir, target_col, ModelClass, params, shuffle = shuffle)
     estimator.parse_files()
     # use only trainers in first wave and later sessions
     mysubjects = [x for x in estimator.subjects if 'lue11' in x ]
@@ -31,21 +38,27 @@ def launch_experiment(files_dir, target_col, ModelClass, params = None):
 #print(mean_score)
 # make experiments
 
-params_list = loop_params(PARAM_GRID, sample = 300)
-score_list = Parallel(n_jobs = NUM_CORES, 
-#                      require = "sharedmem", 
-                      verbose = 0)(delayed(launch_experiment) 
-                                         (FILES_DIR, TARGET_COL, SiameseNet, 
-                                          params)
-                                          for params in params_list)
+#if __name__ == '__main__':
+launch_experiment(FILES_DIR, TARGET_COL, SiameseNet, DEFAULT_SIAMESE_PARAMS, shuffle)
 
-best_score = max(score_list)
-best_index = score_list.index(best_score)
-best_params = params_list[best_index]
-print(best_score)
-print(best_params)
-results = {'score_list' : score_list, 'params_list' : params_list}
+if False:                                         
+    params_list = loop_params(PARAM_GRID)
+    score_list = Parallel(n_jobs = NUM_CORES, 
+    #                      require = "sharedmem", 
+                          verbose = 0)(delayed(launch_experiment) 
+                                             (FILES_DIR, TARGET_COL, SiameseNet, 
+                                              params, shuffle)
+                                              for params in params_list)
+    
+    results = {'score_list' : score_list, 'params_list' : params_list}
+    best_score = max(results['score_list'])
+    best_index = results['score_list'].index(best_score)
+    best_params = results['params_list'][best_index]
+    print(best_score)
+    print(best_params)
 
-import pickle
-with open(os.path.join(OUTPUT_DIR, 'results.pkl'), 'wb') as output:
-    pickle.dump(results, output, pickle.HIGHEST_PROTOCOL)
+    with open(results_file, 'wb') as output:
+        pickle.dump(results, output, pickle.HIGHEST_PROTOCOL)   
+        
+plot_results(results_file)
+
