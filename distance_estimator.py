02546a4709fac5723a39f6f12ef2e82497033054
Benjamín Garzón <benjamin.garzon@gmail.com>
2021-07-20 12:53:59 +0200

    logging, only same session

--- distance_estimator.py
+++ distance_estimator.py
@@ -12,6 +12,7 @@ Created on Wed Jul 14 12:59:49 2021
 import logging, os, glob
 import pandas as pd
 import numpy as np
+from random import choice
 from collections import defaultdict
 from datetime import date, datetime
 today = date.today().isoformat()
@@ -19,15 +20,17 @@ now = datetime.now().isoformat()
 
 class Estimator():
 
-    def __init__(self, files_dir, target_col, ModelClass, params = None):
+    def __init__(self, files_dir, target_col, ModelClass, params = None, 
+                 shuffle = False):
         self.logger = logging.getLogger()
         self.logger.setLevel(logging.INFO)
-        handler = logging.FileHandler(f"logs/log-{now}.log", 'a', 'utf-8')
+        handler = logging.FileHandler(f"logs/log-{today}.log", 'a', 'utf-8')
         self.logger.addHandler(handler)
         self.files_dir = files_dir
         self.Model = ModelClass
         self.target_col = target_col
         self.params = params
+        self.shuffle = shuffle
     
     def parse_files(self):
         """
@@ -67,7 +70,8 @@ class Estimator():
         print("Found {} subjects: {}".format(len(self.subjects), self.subjects))
 
               
-    def estimate(self, subjects = None, labels = None, selected_sessions = None):
+    def estimate(self, subjects = None, labels = None, selected_sessions = None, 
+                 only_same_session = False):
         """
         Run through the lists and estimate the parameters. These are saved in 
         a dict (label, sess_train, sess_test, seq_class).
@@ -93,6 +97,9 @@ class Estimator():
             for subject in subjects: # could be sampled
                 mysessions = sessions[subject] if selected_sessions is None \
                     else [ x for x in sessions[subject] if x in selected_sessions ]
+                    
+                if len(mysessions) < 2: 
+                    continue
 
                 for session_train in mysessions:
 
@@ -100,23 +107,45 @@ class Estimator():
                     #                                              label, 
                     #                                              session_train))
 
-                    print((subject, label, session_train))
-                    model = self.Model(self.target_col, self.params)
-                    model.fit_file(self.files[(subject, label, session_train)])
-                    
+                    #print((subject, label, session_train))
+                    model = self.Model(self.target_col, 
+                                       model_ref = 'exp_label-{}_sub-{}_ses-{}-{}'.format(
+                                           label, 
+                                           subject, 
+                                           session_train, 
+                                           self.params['exp_num']), 
+                                       params = self.params,
+                                       logger = self.logger,
+                                       shuffle = self.shuffle)
+                    # ad one randomly selected test session
+                    session_test = choice([ x for x in mysessions if x != session_train ])
+                    train_file = self.files[(subject, label, session_train)]
+                    test_file = self.files[(subject, label, session_test)]
+                    if only_same_session:
+                        cv_score = model.fit_and_predict_file_cv(train_file)
+                    if not only_same_session:
+                        model.fit_file(train_file, test_file)
                     # now test in other sessions
                     for session_test in mysessions: # add type as well
                     
                             index = (label, session_train, session_test)
-                            score = model.predict_file(
-                                self.files[(subject, label, session_test)])
+                            
                             if session_test != session_train: 
-                                scores[index].append(score)
-                            #else:
-                            #    if score > 1.5:
-                            #        self.logger.info("Scores on training:{}, {} \n Params:{}".format(index, score, self.params))
+                                if not only_same_session:
+                                    score = model.predict_file(
+                                        self.files[(subject, label, session_test)])
+                                    scores[index].append(score)
+                                    self.logger.info("Scores on training:{}, {}, {} \n Params:{}".format(subject, index, scores[index], self.params))
 
-        mean_score = np.mean([np.mean(x) for x in scores.values()])
-        if mean_score > 1.1:
-            self.logger.info("Mean scores on test: {} \n Params:{}".format(mean_score, self.params))
+                            else:
+                                if only_same_session: 
+                                    scores[index].append(cv_score)
+                                    self.logger.info("Scores on training:{}, {}, {} \n Params:{}".format(subject, index, scores[index], self.params))
+
+                                #if cv_score > 10:
+
+        mean_score = np.mean([np.nanmean(x) for x in scores.values()])
+#        self.logger.info("Finished experiment, Params:{}".format(self.params))
+        if mean_score > 1:
+            self.logger.info("Mean scores on test: {}; Params:{}".format(mean_score, self.params))
         return(scores, mean_score)
