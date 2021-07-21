cd6f622f4af2301ac12f98bdf22278bc84939ea8
Benjamín Garzón <benjamin.garzon@gmail.com>
2021-07-21 13:18:51 +0200

    parameter update

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ Created on Thu Jul 15 10:46:49 2021
 import os 
 FILES_DIR = '/data/lv0/MotorSkill/fmriprep/analysis/roi_data'
 TARGET_COL = 'true_sequence' # 'seq_type'
-TRAIN_FILE = '/data/lv0/MotorSkill/fmriprep/analysis/roi_data/sub-lue1105_ses-5_roi_data_mask_rh_R_SPL.csv'
-TEST_FILE = '/data/lv0/MotorSkill/fmriprep/analysis/roi_data/sub-lue1105_ses-6_roi_data_mask_rh_R_SPL.csv'
+TRAIN_FILE = '/data/lv0/MotorSkill/fmriprep/analysis/roi_data/sub-lue1107_ses-5_roi_data_mask_rh_R_SPL.csv'
+TEST_FILE = '/data/lv0/MotorSkill/fmriprep/analysis/roi_data/sub-lue1107_ses-6_roi_data_mask_rh_R_SPL.csv'
 OUTPUT_DIR = '/data/lv0/MotorSkill/fmriprep/analysis/results'
 
-#MODEL_PATH = os.path.sep.join([OUTPUT, "siamese_model"])
+MODEL_PATH = os.path.join(OUTPUT_DIR, "models")
 PLOT_PATH = os.path.join(OUTPUT_DIR, "plots")
 
 
@@ -23,25 +23,29 @@ TEST_LABEL = ['R_SPL']
 NUM_CORES = 30
 
 DEFAULT_SIAMESE_PARAMS = {
-    'n1' : 50,
+    'n1' : 10,
     'n2' : 0,
-    'dropout' : 0.2,
-    'activation' : 'tanh',
+    'dropout' : 0,
+    'activation' : None,
     'embedding_dimension' : 10,
     'loss' : 'contrastive_loss',
     'batch_size' : 64,
-    'epochs' : 30,
-    'exp_num' : 0
+    'epochs' : 100,
+    'exp_num' : '0000',
+    'scaling' : 1.0,
+    'learning_rate': 0.001
     }
 
 PARAM_GRID = {
-    'n1' : [20, 50, 100, 200],
+    'n1' : [10, 50, 100],
     'n2' : [0, 20, 50],
     'dropout' : [0, 0.2],
-    'activation' : ['tanh', 'sigmoid'],# 'elu'],
+    'activation' : ['relu', 'sigmoid', 'tanh'],# 'elu'],
     'embedding_dimension' : [5, 10, 15],
     'loss' : ['contrastive_loss'], #'binary_crossentropy', 
     'batch_size' : [64], #32
-    'epochs' : [50] #, 100]
+    'epochs' : [100], 
+    'scaling' : [1.0, 500.0],
+    'learning_rate': [0.01, 1e-4]
 
 }
