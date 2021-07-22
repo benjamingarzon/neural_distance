327fccebcde3932495ffebf35793c4d0544860db
Benjamín Garzón <benjamin.garzon@gmail.com>
2021-07-22 10:51:47 +0200

    losses in new file

---
+++ losses.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 13:28:59 2021
+
+@author: xgarzb@GU.GU.SE
+"""
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+
+def triplet_loss(anchor, positive, negative, margin = 1):
+    dist_pos = tf.sqrt(tf.reduce_sum(tf.pow(anchor - positive, 2), 1, 
+                                  keepdims=True))
+    dist_neg = tf.sqrt(tf.reduce_sum(tf.pow(anchor - negative, 2), 1, 
+                                  keepdims=True))
+    return tf.reduce_mean(tf.maximum(dist_pos - dist_neg + margin, 0)) 
+
+
+def contrastive_loss(y_true, y_pred):
+    # y_true is 1 for equal labels!
+    margin = 1.0
+
+    square_pred = tf.math.square(y_pred)
+
+    margin_square = tf.math.square(tf.math.maximum(margin - y_pred, 0))
+    return tf.math.reduce_mean(y_true * square_pred + 
+                               (1 - y_true) * margin_square)
+
